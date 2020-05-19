59878d7a5743028f9ff1528efce41dfe7daa0767
ivaben <ivaben@users.noreply.github.com>
2020-05-19 12:00:21 -0400

    finished assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,14 +2,16 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+       smallest_index = i
+       for j in range(i, len(arr)):
+           if arr[smallest_index] > arr[j]:
+            smallest_index = j
 
 
         # TO-DO: swap
+       arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+
+
         # Your code here
 
     return arr
@@ -17,15 +19,21 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    for i in range(1, len(arr)):
+        curr_pos = arr[i]
+        j = i
+        while j > 0 and curr_pos < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            j -= 1
 
+    
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort(arr, maximum=-1):
+#def count_sort(arr, maximum=-1):
     # Your code here
 
 
-    return arr
+ #   return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(len(arr)):
+        if(arr[i] == target):
+            return i
 
-
+    
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    # let's figure out the total size of the arr
+    low = 0
+    high = len(arr) - 1
 
-
-    return -1  # not found
+    while low <= high:
+        mid = (low + high) // 2 # find the mid point
+        if arr[mid] > target: high = mid-1
+        elif arr[mid] < target: low = mid+1
+        else: return mid
+        
+        
+    return -1  
